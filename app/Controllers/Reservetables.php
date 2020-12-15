86e589700d39d12e3ef4fb6c179b1e1f133df7b5
bank <panupong4444@hotmail.com>
2020-12-15 20:54:07 +0700

    bank edit

--- app/Controllers/Reservetables.php
+++ app/Controllers/Reservetables.php
@@ -17,6 +17,38 @@ class Reservetables extends BaseController
 		return view('floor2_view');
 	}
 
+	public function zone1()
+	{
+		return view('Zone1');
+	}
+	public function zone2()
+	{
+		return view('Zone2');
+	}
+	public function zone3()
+	{
+		return view('Zone3');
+	}
+	public function zone4()
+	{
+		return view('Zone4');
+	}
+	public function zone5()
+	{
+		return view('Zone5');
+	}
+	public function zone6()
+	{
+		return view('Zone6');
+	}
+	public function zone7()
+	{
+		return view('Zone7');
+	}
+	public function zone8()
+	{
+		return view('Zone8');
+	}
 	public function view($page = 'home')
 	{
 		
--- app/Views/floor_view.php
+++ app/Views/floor_view.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<title>CIT</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="<?php echo base_url('bootstrap/css/bootstrap.min.css')?>" type="text/css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script type="text/javascript" src="<?php echo base_url('bootstrap/js/bootstrap.min.js')?>"><</script>
