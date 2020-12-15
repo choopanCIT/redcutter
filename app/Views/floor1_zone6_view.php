a6eaaace1a16120ea2bd5a6a2f2ee33b11c1e51b
bank <panupong4444@hotmail.com>
2020-12-16 05:35:07 +0700

    add all file floor2,floor1 and connect data base for check seat available

---
+++ app/Views/floor1_zone6_view.php
@@ -0,0 +1,180 @@
+<!doctype html>
+
+<html>
+	<head>
+		<title>JSC Demo</title>
+		<meta charset="UTF-8">
+		<link href='https://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url('seatchart/style.css')?>">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+		<script type="text/javascript" src="<?php echo base_url('bootstrap/js/bootstrap.min.js')?>"></script>
+	</head>
+
+	<body>
+	<div class="jumbotron text-center">
+	<h2>The red cutter day</h2>
+	<p>The way walk you home.</p>
+	<a href="<?php echo site_url('home'); ?>" class="btn btn-primary">กลับหน้าหลัก</a></p>
+	</div>
+		<div class="container">
+			<div class="row justify-content-center">
+		 <div class="col-8 col-sm-7">
+			<div class="col-12 col-sm-12">
+				<div class="row justify-content-center">
+					<div class="col-6 col-sm d-flex justify-content-center justify-content-md-center" style="display: none;"></div>
+					<div class="col-5 col-sm-6 d-flex justify-content-center justify-content-md-center" id="Zone6-1" name="366">366</div>
+				</div>
+		</div>
+		<div class="col-12 col-sm-12">
+			<div class="row justify-content-center">
+				<div class="col-5 col-sm-6 d-flex justify-content-center justify-content-md-center" id="Zone6-1" name="364">364</div>
+				<div class="col-6 col-sm d-flex justify-content-center justify-content-md-center" style="display: none;"></div>
+			</div>
+	</div>
+	<div class="col-12 col-sm-12">
+		<div class="row justify-content-center">
+			<div class="col-6 col-sm d-flex justify-content-center justify-content-md-center" style="display: none;" ></div>
+			<div class="col-5 col-sm-6 d-flex justify-content-center justify-content-md-center" id="Zone6-1" name="361">361</div>
+		</div>
+</div>
+<div class="col-12 col-sm-12">
+	<div class="row justify-content-center">
+		<div class="col-5 col-sm-6 d-flex justify-content-center justify-content-md-center" id="Zone6-1" name="358">358</div>
+		<div class="col-6 col-sm d-flex justify-content-center justify-content-md-center" style="display: none;"></div>
+	</div>
+</div>
+<div class="col-12 col-sm-12">
+	<div class="row justify-content-center">
+		<div class="col-6 col-sm d-flex justify-content-center justify-content-md-center" style="display: none;"></div>
+		<div class="col-5 col-sm-6 d-flex justify-content-center justify-content-md-center" id="Zone6-1" name="355">355</div>
+	</div>
+</div>
+
+<div class="col-12 col-sm-12">
+	<div class="row justify-content-center">
+		<div class="col-5 col-sm-6 d-flex justify-content-center justify-content-md-center" id="Zone6-1" name="352">352</div>
+		<div class="col-6 col-sm d-flex justify-content-center justify-content-md-center" style="display: none;"></div>
+	</div>
+</div>
+<div class="col-12 col-sm-12">
+	<div class="row justify-content-center">
+		<div class="col-6 col-sm d-flex justify-content-center justify-content-md-center" style="display: none;"></div>
+		<div class="col-5 col-sm-6 d-flex justify-content-center justify-content-md-center" id="Zone6-1" name="349">349</div>
+	</div>
+</div>
+<div class="col-12 col-sm-12">
+	<div class="row justify-content-center">
+		<div class="col-5 col-sm-6 d-flex justify-content-center justify-content-md-center" id="Zone6-1" name="346">346</div>
+		<div class="col-6 col-sm d-flex justify-content-center justify-content-md-center" style="display: none;"></div>
+	</div>
+</div>
+</div>
+<div class="col-sm">
+ <h4 class="d-flex justify-content-between align-items-center mb-3">
+	<span class="text-muted">Your cart</span>
+ </h4>
+<ul class="list-group mb-3" id="cart">
+<li class="list-group-item d-flex justify-content-between">
+ <span>Total (Bath)</span>
+ <strong class="total">฿0</strong>
+</li>
+</ul>
+
+<form class="card p-2" id="fixedElement">
+<div class="col-12">
+ <div class="row justify-content-around">
+	<button type="button" class="col-8 col-sm-8 btn btn btn-success" id="checkout" data-toggle="modal">Checkout(<strong class="total">฿0</strong>)</button>
+	<button type="button" class="col-4 col-sm-4 btn btn btn-warning" id="clear">Clear</button>
+ </div>
+</div>
+</form>
+</div>
+</div>
+<div class="row justify-content-center">
+<div class="col-8 col-sm-6" id="detail">
+	<div class="row justify-content-center">
+	<div class="col-1" id="Zonegreen"></div><span>&nbsp ว่าง&nbsp</span>
+	<div class="col-1" id="Zoneyellow"></div><span>&nbsp จอง&nbsp</span>
+	<div class="col-1" id="Zonered"></div><span> &nbsp จ่ายเงินเเล้ว&nbsp</span>
+</div>
+</div>
+</div>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="ModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+<div class="modal-dialog modal-dialog-centered" role="document">
+ <div class="modal-content">
+	<div class="modal-header">
+		<h5 class="modal-title" id="exampleModalLongTitle">ข้อมูลผู้จอง</h5>
+		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+		</button>
+	</div>
+	<div class="modal-body">
+		<form>
+<div class="form-group">
+ <p></p>
+ <label for="recipient-name" class="col-form-label">ชื่อ:</label>
+ <input type="text" class="form-control" id="recipient-name">
+ <label for="recipient-name" class="col-form-label">นามสกุล:</label>
+ <input type="text" class="form-control" id="recipient-surename">
+ <label for="recipient-name" class="col-form-label">รหัสรุ่น:</label>
+ <input type="text" class="form-control" id="recipient-id">
+ <label for="recipient-name" class="col-form-label">เบอร์โทร:</label>
+ <input type="text" class="form-control" id="recipient-phone">
+</div>
+</form>
+	</div>
+	<div class="modal-footer">
+		<button type="button" class="btn btn-secondary" data-dismiss="modal">ยกเลิก</button>
+		<button type="button" class="btn btn-primary" id="submit">ยืนยันการจอง</button>
+		</div>
+	</div>
+</div>
+</div>
+	<script>
+	$(document).ready(function() {
+		//$('div[id*="Zone"]').seatcheack("1");
+	   $("#checkout").attr("data-target","#ModalCenter");
+		$('#clear').click(function(){
+			location.reload(true);
+		});
+	   $('#submit').click(function(){
+		   var results= $(this).submitdata({});
+		   console.log(results);
+	   });
+   });
+	</script>
+<script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="<?php echo base_url('seatchart/jquery.seat-charts.js')?>"></script>
+<?php 
+//print_r($tabinfo);
+
+foreach ($tabinfo as $row)
+{
+   if ($row->status == 2) {
+	   echo "<script>
+			$('div[name=\"$row->id\"]').seatconfirm({});
+			</script>";
+   }
+   if ($row->status == 1) {
+	   echo "<script>
+			$('div[name=\"$row->id\"]').seatwaitconfirm({});
+			</script>";
+   }
+   if ($row->status == 0) {
+	   //echo $row->id;
+	   echo "<script>
+			$('div[name=\"$row->id\"]').seatavailable($row->price);
+			</script>";
+   }
+   //echo $row->id;
+   //echo $row->zone;
+   //echo $row->floor;
+}
+?>
+</body>
+</html>
---
+++ app/Views/floor2_view.php
@@ -0,0 +1,63 @@
+<!doctype html>
+
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>CIT</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script type="text/javascript" src="<?php echo base_url('bootstrap/js/bootstrap.min.js')?>"><</script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('seatchart/style.css')?>">
+	</head>
+
+	<body>
+	<div class="jumbotron text-center">
+		<h2>The red cutter day</h2>
+		<p>The way walk you home.</p>
+		<a href="<?php echo site_url('home'); ?>" class="btn btn-primary">กลับหน้าหลัก</a></p>
+		</div>
+		<div class="container">
+		<b>จองโต๊ะ (ชั้น 2) :</b> กรุณาเลือก Zone ที่ต้องการจองโต๊ะ<br><br>
+
+			<div class="col-9 col-sm-8" id="front-indicator">STAGE</div>
+			<div class="row justify-content-center justify-content-sm-center">
+			<div class="col-10 col-sm-8">
+			<div class="row justify-content-center justify-content-sm-center">
+		    <div class="col-1 col-sm-1 d-flex justify-content-center justify-content-sm-center" id="Zone6" onclick="location.href='<?php echo site_url('home/tablelayout/2/1'); ?>';">ZONE1</div>
+				<div class="col-1 col-sm-1 d-flex justify-content-center justify-content-sm-center" id="Zone6" onclick="location.href='<?php echo site_url('home/tablelayout/2/2'); ?>';">ZONE2</div>
+				<div class="col-3 col-sm-3">
+					<div class="row justify-content-center">
+				<div class="col-12 col-sm d-flex justify-content-center justify-content-sm-center" id="front-indicator2">2 FLOOR</div>
+			</div>
+		</div>
+		<div class="col-1 col-sm-1 d-flex justify-content-center justify-content-sm-center" id="Zone6" onclick="location.href='<?php echo site_url('home/tablelayout/2/3'); ?>';">ZONE3</div>
+		<div class="col-1 col-sm-1 d-flex justify-content-center justify-content-sm-center" id="Zone6" onclick="location.href='<?php echo site_url('home/tablelayout/2/4'); ?>';">ZONE4</div>
+		<div>
+		</div>
+				<div class="w-100"></div>
+			<div class="col-4 col-sm-4">
+				<div class="row">
+					<div class="col col-sm d-flex justify-content-center justify-content-md-center" id="Zone6" onclick="location.href='<?php echo site_url('home/tablelayout/2/5'); ?>';">ZONE5</div>
+				</div>
+				<div class="row" style="white-space: pre-wrap;">
+					<div class="col d-flex justify-content-center justify-content-md-center" id="Zone4" onclick="location.href='<?php echo site_url('home/tablelayout/2/6'); ?>';">ZONE6</div>
+				</div>
+		</div>
+		 </div>
+		 <div class="w-100"></div>
+		 <div class="row justify-content-center">
+		 <div class="col-12" id="detail">
+			 <div class="row row justify-content-center">
+				 <div class="col-1" id="Zoneyellow"></div><span>&nbsp 6 ที่นั่ง&nbsp</span>
+			 <div class="col-1" id="Zonered"></div><span> &nbsp 6 ที่นั่ง(HUMBURG)&nbsp</span>
+		 </div>
+	 </div>
+		 </div>
+		 </div>
+	</div>
+		<script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+	</body>
+</html>
